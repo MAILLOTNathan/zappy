d826b4b84b6209f7603b044562b973af469e00f6
shookapic <cedric.roulof@epitech.eu>
2024-05-21 16:52:52 +0400

    [ADD] basic ia logic, code refactor

--- ia/debug_lib.py
+++ ia/debug_lib.py
@@ -1,22 +1,46 @@
 import sys
 import socket
 import zappy_ai
+import time
 
-def connect_to_server(HOST, PORT):
-    if HOST == "localhost":
-        HOST = "127.0.0.1"
+class ServerConnection:
+    def __init__(self, ai):
+        if ai.host == "localhost":
+            self.HOST = "127.0.0.1"
+        else:
+            self.HOST = ai.host
+        self.PORT = ai.port
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect((self.HOST, self.PORT))
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
+    def connect_to_server_debug(self):
+        data = self.s.recv(1024)
+        print(repr(data))
         while True:
             input = sys.stdin.readline()
             if input == "quit\n":
                 exit(0)
             try:
-                s.send(input.encode())
-                data = s.recv(1024)
+                self.s.send(input.encode())
+                data = self.s.recv(1024)
                 print(repr(data))
             except:
                 print("Error: Connection to server lost.")
                 exit(84)
-        
+
+    def connect_to_server(self, team_name):
+        data = self.s.recv(1024)
+        print(repr(data))
+        self.s.sendall((team_name + "\n").encode())
+        data = self.s.recv(1024)
+        print(repr(data))         
+
+    def send_request(self, request):
+        try:
+            self.s.send((request + "\n").encode())
+            data = self.s.recv(1024)
+            print(repr(data))
+            return data
+        except:
+            print("Error: Connection to server lost.")
+            exit(84)
--- ia/zappy_ai.py
+++ ia/zappy_ai.py
@@ -14,16 +14,17 @@ class TuringAI:
         command (list): The list of available commands for the AI agent.
     """
     debug = False
-    port = 0
+    port = None
     team_name = ""
     host = "localhost"
     command = ["Forward", "Right", "Left", "Look", "Inventory", "Broadcast", "Connect_nbr", "Fork", "Eject", "Take", "Set", "Incantation"]
     def __init__(self):
         self.debug = False
-        self.port = 0
+        self.port = None
         self.team_name = ""
         self.host = "localhost"
         self.command = ["Forward", "Right", "Left", "Look", "Inventory", "Broadcast", "Connect_nbr", "Fork", "Eject", "Take", "Set", "Incantation"]
+        self.inventory = {"food": 0, "linemate": 0, "deraumere": 0, "sibur": 0, "mendiane": 0, "phiras": 0, "thystame": 0}
 
 def help_message():
     """
@@ -32,7 +33,7 @@ def help_message():
     print("USAGE: ./zappy_ai -p port -n name -h machine")
     exit(0)
 
-def check_args():
+def check_args(TuringAI):
     """
     Check the command line arguments and perform necessary actions based on the arguments.
 
@@ -50,20 +51,133 @@ def check_args():
             TuringAI.port = int(sys.argv[i + 1])
         elif sys.argv[i] == "-n":
             TuringAI.team_name = sys.argv[i + 1]
+            if TuringAI.team_name == "GRAPHIC":
+                print("Error: Team name cannot be GRAPHIC.")
+                exit(84)
         elif sys.argv[i] == "-h":
             TuringAI.host = sys.argv[i + 1]
     if (len(sys.argv) < 2) or sys.argv[1] == "-help":
         help_message()
     if sys.argv[1] == "--debug":
         TuringAI.debug = True
-        debug_lib.connect_to_server(TuringAI.host, TuringAI.port)
+    if TuringAI.port == None or TuringAI.team_name == "":
+        help_message()
+
+def parse_look(response):
+    """
+    Parse the look response from the server and return the tile where there is the most food.
+
+    Args:
+        response (str): The response from the server.
+
+    Returns:
+        str: The direction where there is the most food.
+    """
+    if 'food' not in response:
+        return ''
+    tiles = response.strip('[]').split(',')
+    food_counts = [tile.count('food') for tile in tiles]
+    max_index = food_counts.index(max(food_counts))
+    return max_index    
+
+def find_path(direction, conn):
+    """
+    Find the path to the tile with the most food.
+
+    Args:
+        direction (str): The direction where there is the most food.
+
+    Returns:
+        None
+    """
+    if direction == 0:
+        print("direction 0")
+        conn.send_request("Take food")
+        print("Food taken")
+    elif direction == 1:
+        print("direction 1")
+        conn.send_request("Forward")
+        conn.send_request("Left")
+        conn.send_request("Forward")
+        conn.send_request("Take food")
+        print("Food taken")
+    elif direction == 2:
+        print("direction 1")
+        conn.send_request("Forward")
+        conn.send_request("Take food")
+        print("Food taken")
+    elif direction == 3:
+        print("direction 2")
+        conn.send_request("Forward")
+        conn.send_request("Right")
+        conn.send_request("Forward")
+        conn.send_request("Take food")
+        print("Food taken")
+    elif direction == 4:
+        print("direction 3")
+        conn.send_request("Forward")
+        conn.send_request("Forward")
+        conn.send_request("Left")
+        conn.send_request("Forward")
+        conn.send_request("Forward")
+        conn.send_request("Take food")
+        print("Food taken")
+    elif direction == 5:
+        print("direction 4")
+        conn.send_request("Forward")
+        conn.send_request("Forward")
+        conn.send_request("Left")
+        conn.send_request("Forward")
+        conn.send_request("Take food")
+        print("Food taken")
+    elif direction == 6:
+        print("direction 5")
+        conn.send_request("Forward")
+        conn.send_request("Forward")
+        conn.send_request("Take food")
+        print("Food taken")
+    elif direction == 7:
+        print("direction 6")
+        conn.send_request("Forward")
+        conn.send_request("Forward")
+        conn.send_request("Right")
+        conn.send_request("Forward")
+        conn.send_request("Take food")
+        print("Food taken")
+    elif direction == 8:
+        print("direction 7")
+        conn.send_request("Forward")
+        conn.send_request("Forward")
+        conn.send_request("Right")
+        conn.send_request("Forward")
+        conn.send_request("Forward")
+        conn.send_request("Take food")
+        print("Food taken")
+
+def basic_ia(conn):
+    while True:
+        for i in range(0, 4):
+            conn.send_request("Right")
+            response = conn.send_request("Look")
+            most_food_case = parse_look(response.decode())
+            if most_food_case == '' :
+                continue
+            find_path(most_food_case, conn)
+            continue
+        conn.send_request("Forward")
 
 def main():
     """
     This is the main function of the Zappy AI program.
     It is responsible for checking the arguments and executing the necessary logic.
     """
-    check_args()
+    ai = TuringAI()
+    check_args(ai)
+    conn = debug_lib.ServerConnection(ai)
+    if ai.debug:
+        conn.connect_to_server_debug()
+    conn.connect_to_server(ai.team_name)
+    basic_ia(conn)
 
 if __name__ == "__main__":
     main()
