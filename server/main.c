74b81e04a1a6c60eb9e5833cef1fe813ce7ac72e
Neisan <nathan.maillot@epitech.eu>
2024-05-21 21:02:45 +0400

    feat: add clock

--- server/main.c
+++ server/main.c
@@ -7,6 +7,7 @@
 
 #include "common_defines.h"
 #include "amber_process.h"
+#include "amber_clock.h"
 #include "signal.h"
 
 amber_serv_t *server;
@@ -21,15 +22,18 @@ void handle_sigint(int sig)
 
 int main(int ac, char **av)
 {
-    args_t args = {0};
+    (void)ac;
+    (void)av;
+    // args_t args = {0};
 
-    signal(SIGINT, handle_sigint);
-    if (amber_check_arg(ac, av, &args) == false)
-        return 84;
-    amber_display_args(&args);
-    server = amber_create_server(&args);
-    if (!server)
-        return 84;
-    amber_listening(server);
+    // signal(SIGINT, handle_sigint);
+    // if (amber_check_arg(ac, av, &args) == false)
+    //     return 84;
+    // amber_display_args(&args);
+    // server = amber_create_server(&args);
+    // if (!server)
+    //     return 84;
+    // amber_listening(server);
+    show_time();
     return 0;
 }
