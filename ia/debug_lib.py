9f382adb7ea49534990b442c81239a03ee04e4a5
shookapic <cedric.roulof@epitech.eu>
2024-05-23 13:16:50 +0400

    [ADD] other reward for AI training, fix documentation

--- ia/debug_lib.py
+++ ia/debug_lib.py
@@ -14,6 +14,20 @@ class ServerConnection:
         self.s.connect((self.HOST, self.PORT))
 
     def connect_to_server_debug(self):
+        """
+        Connects to the server in debug mode.
+
+        This method establishes a connection to the server and allows the user to send and receive data.
+        It continuously listens for user input and sends it to the server. If the user enters "quit",
+        the program exits. If there is an error in the connection, an error message is printed and the
+        program exits with an exit code of 84.
+
+        Args:
+            self: The instance of the class.
+
+        Returns:
+            None
+        """
         data = self.s.recv(1024)
         print(repr(data))
         while True:
@@ -29,18 +43,43 @@ class ServerConnection:
                 exit(84)
 
     def connect_to_server(self, team_name):
+        """
+        Connects to the server and sends the team name.
+
+        Args:
+            team_name (str): The name of the team.
+
+        Returns:
+            None
+        """
         data = self.s.recv(1024)
         print(repr(data))
         self.s.sendall((team_name + "\n").encode())
         data = self.s.recv(1024)
-        print(repr(data))         
+        print(repr(data))
 
     def send_request(self, request):
-        try:
-            self.s.send((request + "\n").encode())
-            data = self.s.recv(1024)
-            print(repr(data))
-            return data
-        except:
-            print("Error: Connection to server lost.")
-            exit(84)
+            """
+            Sends a request to the server and returns the response.
+
+            Args:
+                request (str): The request to send to the server.
+
+            Returns:
+                bytes: The response received from the server.
+
+            Raises:
+                SystemExit: If there is an error in the connection to the server.
+            """
+            print("AI request: ", request)
+            try:
+                self.s.send((request + "\n").encode())
+                data = self.s.recv(1024)
+                print(repr(data))
+                if data == b"dead\n":
+                    print("AI is dead.")
+                    return "dead"
+                return data
+            except:
+                print("Error: Connection to server lost.")
+                exit(84)
