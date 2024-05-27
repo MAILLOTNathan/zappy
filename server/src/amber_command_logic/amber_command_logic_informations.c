30dff7d8c33db1d5f317a1f60e6a6576cd8fb98f
Neisan <nathan.maillot@epitech.eu>
2024-05-27 19:29:01 +0400

    fix: messages for Look

--- server/src/amber_command_logic/amber_command_logic_informations.c
+++ server/src/amber_command_logic/amber_command_logic_informations.c
@@ -6,8 +6,17 @@
 */
 
 #include "amber_logic.h"
+#include "string_array.h"
 
-static void amber_look_up(amber_client_t *client, amber_world_t *world)
+static char *switch_string(amber_world_t *world, int y, int x)
+{
+    char *tmp = amber_world_case_stringify(&world->_case[y][x]);
+
+    tmp = !tmp ? strdup(",") : tmp;
+    return tmp;
+}
+
+static char **amber_look_up(amber_client_t *client, amber_world_t *world)
 {
     int level = client->_level;
     int start_y = clamp(0, client->_y - level, world->_height);
@@ -15,19 +24,22 @@ static void amber_look_up(amber_client_t *client, amber_world_t *world)
     int offset = 0;
     int x = 0;
     int y = 0;
+    char **result = NULL;
 
     for (int i = level; i > 0; i--) {
         y = clamp(0, start_y + offset, world->_height);
         y = y == world->_height ? 0 : y;
         for (int j = 0; i * 2 + 1 > j; j++) {
-            x = clamp(0, start_x - j, world->_width);
-            printf("x: %d, y: %d\n", x, y);
+            x = clamp(0, start_x + j, world->_width);
+            x = x == world->_width ? 0 : x;
+            result = append_string_array(result, switch_string(world, y, x));
         }
         offset++;
     }
+    return result;
 }
 
-static void amber_look_left(amber_client_t *client, amber_world_t *world)
+static char **amber_look_left(amber_client_t *client, amber_world_t *world)
 {
     int level = client->_level;
     int start_y = clamp(0, client->_y - level, world->_height);
@@ -35,6 +47,7 @@ static void amber_look_left(amber_client_t *client, amber_world_t *world)
     int offset = 0;
     int x = 0;
     int y = 0;
+    char **result = NULL;
 
     for (int i = level; i > 0; i--) {
         x = clamp(0, start_x + offset, world->_width);
@@ -42,13 +55,14 @@ static void amber_look_left(amber_client_t *client, amber_world_t *world)
         for (int j = 0; i * 2 + 1 > j; j++) {
             y = clamp(0, start_y + j, world->_height);
             y = y == world->_height ? 0 : y;
-            printf("x: %d, y: %d\n", x, y);
+            result = append_string_array(result, switch_string(world, y, x));
         }
         offset++;
     }
+    return result;
 }
 
-static void amber_look_down(amber_client_t *client, amber_world_t *world)
+static char **amber_look_down(amber_client_t *client, amber_world_t *world)
 {
     int level = client->_level;
     int start_y = clamp(0, client->_y + level, world->_height);
@@ -56,6 +70,7 @@ static void amber_look_down(amber_client_t *client, amber_world_t *world)
     int offset = 0;
     int x = 0;
     int y = 0;
+    char **result = NULL;
 
     for (int i = level; i > 0; i--) {
         y = clamp(0, start_y - offset, world->_height);
@@ -63,49 +78,57 @@ static void amber_look_down(amber_client_t *client, amber_world_t *world)
         for (int j = 0; i * 2 + 1 > j; j++) {
             x = clamp(0, start_x + j, world->_width);
             x = x == world->_width ? 0 : x;
-            printf("x: %d, y: %d\n", x, y);
+            result = append_string_array(result, switch_string(world, y, x));
         }
         offset++;
     }
+    return result;
 }
 
-static void amber_look_right(amber_client_t *client, amber_world_t *world)
+static char **amber_look_right(amber_client_t *client, amber_world_t *world)
 {
-    int level = client->_level;
-    int start_y = clamp(0, client->_y - level, world->_height);
-    int start_x = clamp(0, client->_x + level, world->_width);
+    int start_y = clamp(0, client->_y - client->_level, world->_height);
+    int start_x = clamp(0, client->_x + client->_level, world->_width);
     int offset = 0;
     int x = 0;
     int y = 0;
+    char **result = NULL;
 
-    printf("start_x: %d, start_y: %d\n", start_x, start_y);
-    for (int i = level; i > 0; i--) {
+    for (int i = client->_level; i > 0; i--) {
         x = clamp(0, start_x - offset, world->_width);
         x = x == world->_width ? 0 : x;
         for (int j = 0; i * 2 + 1 > j; j++) {
             y = clamp(0, start_y + j, world->_height);
             y = y == world->_height ? 0 : y;
-            printf("x: %d, y: %d\n", x, y);
+            result = append_string_array(result, switch_string(world, y, x));
         }
         offset++;
     }
+    return result;
 }
 
 void amber_logic_look(amber_client_t *client, amber_world_t *world)
 {
+    char **result = NULL;
+    char *response = NULL;
+
     switch (client->_direction) {
         case UP:
-            amber_look_up(client, world);
+            result = amber_look_up(client, world);
             break;
         case LEFT:
-            amber_look_left(client, world);
+            result = amber_look_left(client, world);
             break;
         case DOWN:
-            amber_look_down(client, world);
+            result = amber_look_down(client, world);
             break;
         case RIGHT:
-            amber_look_right(client, world);
+            result = amber_look_right(client, world);
     }
+    response = string_array_to_string_separator(result, ',');
+    dprintf(client->_tcp._fd, "[%s]\n", response);
+    free_string_array(result);
+    free(response);
 }
 
 void amber_logic_inventory(amber_client_t *client, UNUSED amber_world_t *world)
