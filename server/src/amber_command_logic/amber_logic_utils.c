6b65514955807aa5bfc332123a20beb2a0c1f40f
shookapic <cedric.roulof@epitech.eu>
2024-06-10 18:30:12 +0400

    [FIX] wrong variable

--- server/src/amber_command_logic/amber_logic_utils.c
+++ server/src/amber_command_logic/amber_logic_utils.c
@@ -69,7 +69,7 @@ static void check_clock_food(amber_client_t *client, amber_world_t *world,
         drop_item(world, client->_x, client->_y, client->_inventory);
         printf("[AMBER INFO] Client %d died\n", client->_tcp._fd);
         dprintf(client->_tcp._fd, "dead\n");
-        amber_event_pdi(client, server);
+        amber_event_pdi(client, server->_graphic_clients);
         world->_case[client->_y][client->_x]._players--;
         remove_node(&server->_clients, node, true);
     } else
