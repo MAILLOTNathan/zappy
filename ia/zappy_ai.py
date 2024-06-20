4dcbec0c48082137a431046906fc61b5be7ac96f
shookapic <cedric.roulof@epitech.eu>
2024-06-20 10:59:16 +0000

    [FIX] missing file

--- ia/zappy_ai.py
+++ ia/zappy_ai.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import sys
+import setup_utils
 import debug_lib
 import os
 import subprocess
@@ -328,44 +328,6 @@ class TuringAI:
         }
         self.broadcast_key = ""
 
-def help_message():
-    """
-    Prints the usage message for the zappy_ai script.
-    """
-    print("USAGE: ./zappy_ai -p port -n name -h machine")
-    exit(0)
-
-def check_args(TuringAI):
-    """
-    Check the command line arguments and perform necessary actions based on the arguments.
-
-    If the number of arguments is less than 2 or the first argument is "-help", it calls the help_message function.
-    If the first argument is "--debug", it sets the debug flag to True and sends a debug request.
-
-    Args:
-        None
-
-    Returns:
-        None
-    """
-    for i in range(1, len(sys.argv)):
-        if sys.argv[i] == "-p":
-            TuringAI.port = int(sys.argv[i + 1])
-        elif sys.argv[i] == "-n":
-            TuringAI.team_name = sys.argv[i + 1]
-            if TuringAI.team_name == "GRAPHIC":
-                print("Error: Team name cannot be GRAPHIC.")
-                exit(83)
-        elif sys.argv[i] == "-h":
-            TuringAI.host = sys.argv[i + 1]
-    if (len(sys.argv) < 2) or sys.argv[1] == "-help":
-        help_message()
-    if sys.argv[1] == "--debug":
-        TuringAI.debug = True
-        return
-    if TuringAI.port == None or TuringAI.team_name == "":
-        help_message()
-
 def get_obj(map, obj):
     x = 0
     y = 0
@@ -510,7 +472,7 @@ def main():
     It is responsible for checking the arguments and executing the necessary logic.
     """
     ai : TuringAI = TuringAI()
-    check_args(ai)
+    setup_utils.check_args(ai)
     conn = debug_lib.ServerConnection(ai)
     ai.broadcast_key = base64.b64encode(ai.team_name.encode()).decode()
     if ai.debug:
